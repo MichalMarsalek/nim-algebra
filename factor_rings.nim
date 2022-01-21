d607bd74c4c078534c9f42fd1c95ef0c4cbd1566
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2022-01-22 00:16:04 +0100

    fix factor rings

--- factor_rings.nim
+++ factor_rings.nim
@@ -16,28 +16,16 @@ func `$`*(p:Ideal):string =
     "I(" & $p.generator & ")"
 
 
-template `+`*[TT](poly:TT, M:static[Ideal[TT]]):FactorRing[TT,M] =
-    result.val = poly
-
-{.push checks: off.}
-
-
-
-func `+++`*[TT](poly:TT, M:static TT):FactorRing[TT, M] =
-    result.val = poly
+func `+`*[TT](poly:TT, M:static[Ideal[TT]]):auto =
+    FactorRing[TT,M.generator](val: poly)
 
 func `+`*[TT, M](f,g:FactorRing[TT,M]):FactorRing[TT, M] =
-    mixin `+`, `mod`
     result.val = (f.val + g.val) mod M
 
-{.pop.}
 func `$`*[TT, M](f:FactorRing[TT, M]):string =
     $f.val & " + I(" & $M & ")"
 
 when isMainModule:
     type R = PR(ZZ,x)
-    const t = x^3
-    let a = x +++ t
-    #let b = x+1 + I(t)
-    #echo a+b
-    echo typeof(I(x^2))
+    let b = x+1 + I(x^3)
+    echo b
